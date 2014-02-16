d18e10aa7f53e45521f174fd312aa79977afe36a
DickingAround <charlie_d_s@hotmail.com>
2014-02-15 17:47:21 -0800

    About 1/3 of the way through the new metrics gathering code. Did a lot of refactoring of simulation monitoring program and a lot of things elsewhere will have to change too

--- simulator/simulationMonitor.c
+++ simulator/simulationMonitor.c
@@ -13,6 +13,126 @@ void simulationMonitor_writeTimeStamp(FILE *f) {
   strftime (buffer,80,"%F_%T",timeinfo);
   fprintf(f,"%s",buffer); 
 }
+
+//-----------------------------------------------
+//  The set/get functions for the data structure
+//-----------------------------------------------
+void simulationMonitor_addDecisionsForHash(int hash, int x)
+ sm.smon.perHashMetrics[hash][0] += x;
+int simulationMonitor_getDecisionsForSpecies(int species, int x)
+ return sm.smon.perSpeciesMetrics[hash][0];
+
+void simulationMonitor_addMovesForHash(int hash, int x)
+ sm.smon.perHashMetrics[hash][1] += x;
+int simulationMonitor_getMovesForSpecies(int species, int x)
+ return sm.smon.perSpeciesMetrics[hash][1];
+
+void simulationMonitor_addTurnsForHash(int hash, int x)
+ sm.smon.perHashMetrics[hash][2] += x;
+int simulationMonitor_getTurnsForSpecies(int species, int x)
+ return sm.smon.perSpeciesMetrics[hash][2];
+
+void simulationMonitor_addAttacksForHash(int hash, int x)
+ sm.smon.perHashMetrics[hash][3] += x;
+int simulationMonitor_getAttacksForSpecies(int species, int x)
+ return sm.smon.perSpeciesMetrics[hash][3];
+
+void simulationMonitor_addGrowsForHash(int hash, int x)
+ sm.smon.perHashMetrics[hash][4] += x;
+int simulationMonitor_getGrowsForSpecies(int species, int x)
+ return sm.smon.perSpeciesMetrics[hash][4];
+
+void simulationMonitor_addASexualReplicationsForHash(int hash, int x)
+ sm.smon.perHashMetrics[hash][5] += x;
+int simulationMonitor_getASexualReplicationsForSpecies(int species, int x)
+ return sm.smon.perSpeciesMetrics[hash][5];
+
+void simulationMonitor_addSexualReplicationsForHash(int hash, int x)
+ sm.smon.perHashMetrics[hash][6] += x;
+int simulationMonitor_getSexualReplicationsForSpecies(int species, int x)
+ return sm.smon.perSpeciesMetrics[hash][6];
+
+void simulationMonitor_addFailedMovesForHash(int hash, int x)
+ sm.smon.perHashMetrics[hash][7] += x;
+int simulationMonitor_getFailedMovesForSpecies(int species, int x)
+ return sm.smon.perSpeciesMetrics[hash][7];
+
+void simulationMonitor_addFailedReplicationsForHash(int hash, int x)
+ sm.smon.perHashMetrics[hash][8] += x;
+int simulationMonitor_getFailedReplicationsForSpecies(int species, int x)
+ return sm.smon.perSpeciesMetrics[hash][8];
+
+void simulationMonitor_addFailedAttacksForHash(int hash, int x)
+ sm.smon.perHashMetrics[hash][9] += x;
+int simulationMonitor_getFailedAttacksForSpecies(int species, int x)
+ return sm.smon.perSpeciesMetrics[hash][9];
+
+void simulationMonitor_addFailedGrowsForHash(int hash, int x)
+ sm.smon.perHashMetrics[hash][10] += x;
+int simulationMonitor_getFailedGrowsForSpecies(int species, int x)
+ return sm.smon.perSpeciesMetrics[hash][10];
+
+void simulationMonitor_addKilledBySeedingForHash(int hash, int x)
+ sm.smon.perHashMetrics[hash][11] += x;
+int simulationMonitor_getKilledBySeedingForSpecies(int species, int x)
+ return sm.smon.perSpeciesMetrics[hash][11];
+
+void simulationMonitor_addKilledByAttacksForHash(int hash, int x)
+ sm.smon.perHashMetrics[hash][12] += x;
+int simulationMonitor_getKilledByAttacksForSpecies(int species, int x)
+ return sm.smon.perSpeciesMetrics[hash][12];
+
+void simulationMonitor_addKilledByStarvingForHash(int hash, int x)
+ sm.smon.perHashMetrics[hash][13] += x;
+int simulationMonitor_getKilledByStarvingForSpecies(int species, int x)
+ return sm.smon.perSpeciesMetrics[hash][13];
+
+void simulationMonitor_addAveBrainSizeForHash(int hash, int x)
+ sm.smon.perHashMetrics[hash][14] += x;
+int simulationMonitor_getAveBrainSizeForSpecies(int species, int x)
+ return sm.smon.perSpeciesMetrics[hash][14];
+
+/*void simulationMonitor_addNewBrainForHash(int hash, int x)
+ sm.smon.perHashMetrics[hash][15] += x;
+int simulationMonitor_getNewBrainForSpecies(int species, int x)
+ return sm.smon.perSpeciesMetrics[hash][15];
+*/ //Legacy
+void simulationMonitor_addAveEnergyForHash(int hash, int x)
+ sm.smon.perHashMetrics[hash][16] += x;
+int simulationMonitor_getAveEnergyForSpecies(int species, int x)
+ return sm.smon.perSpeciesMetrics[hash][16];
+
+void simulationMonitor_addAveAgeForHash(int hash, int x)
+ sm.smon.perHashMetrics[hash][17] += x;
+int simulationMonitor_getAveAgeForSpecies(int species, int x)
+ return sm.smon.perSpeciesMetrics[hash][17];
+
+void simulationMonitor_addAveGenerationForHash(int hash, int x)
+ sm.smon.perHashMetrics[hash][18] += x;
+int simulationMonitor_getAveGenerationForSpecies(int species, int x)
+ return sm.smon.perSpeciesMetrics[hash][18];
+
+void simulationMonitor_addNumberOfAgentsForHash(int hash, int x)
+ sm.smon.perHashMetrics[hash][19] += x;
+int simulationMonitor_getNumberOfAgentsForSpecies(int species, int x)
+ return sm.smon.perSpeciesMetrics[hash][19];
+
+//-----------------------
+// The per agent metrics
+//-----------------------
+void simulationMonitor_findPerAgentMetrics() {
+ for(i = 0; i < sm.w.numbAgents; i++) {
+  if(sm.w.agents[i].status == AG_STATUS_ALIVE) {
+   simulationMonitor_addNumberOfAgentsForHash(sm.w.agents[i].br.hash,1);
+   simulationMonitor_addAveGenerationForHash( sm.w.agents[i].br.hash,sm.w.agents[i].generation);
+   simulationMonitor_addAveAgeForHash( sm.w.agents[i].br.hash,sm.w.agents[i].age);
+   simulationMonitor_addAveEnergyForHash( sm.w.agents[i].br.hash,sm.w.agents[i].energy);
+   simulationMonitor_addAveBrainSizeForHash(sm.w.agents[i].br.hash,sm.w.agents[i].);
+  }
+ }
+}
+ 
+
  //-----------------------------------------------------------
  //  Detect the species so we can collect the data into them. 
  //-----------------------------------------------------------
@@ -78,7 +198,7 @@ bool simulationMonitor_findSpecies(int **speciesList) { //SpeciesList[SPECIES_MA
  }
  if(!waitingToSeeNewSpecies) { //Didn't find a species??
   printf("Simulation Monitor: Didn't ever find the beginning of a new species...\n"); 
-  return false; 
+  return 0; 
  }
  //Detect species as we go
  for(i=startingSpot; i < AG_MAX_HASH + SPECIES_DETECTION_POPULATION_DEF_MIN) {
@@ -100,31 +220,92 @@ bool simulationMonitor_findSpecies(int **speciesList) { //SpeciesList[SPECIES_MA
    speciesList[speciesNumber][2] += c;
   }
  }
- return true;
+ return speciesNumber;
+}
+
+void simulationMonitor_collectSpeciesMetrics(int **speciesList, int numberOfSpecies) {
+ int speciesNumber,metricNumber,hashNumber;
+ for(speciesNumber=0; speciesNumber < numberOfSpecies; speciesNumber++) {
+  //Clear the species metrics from last time
+  for(metricNumber =0; metricNumber < SPECIES_NUMBER_OF_METRICS; metricNumber++) {
+   sm.smon.perSpeciesMetrics[speciesNumber][metricNumber] = 0; 
+  } 
+  //Compute the species metrics by adding up the metrics from all the hashes in that range
+  for(hashNumber = speciesList[0]; hashNumber < speciesList[1]; hashNumber++) {
+   for(metricNumber=0; metricNumber < SPECIES_NUMBER_OF_METRICS; metricNumber++) {
+    sm.smon.perSpeciesMetrics[speciesNumber][metricNumber] += sm.smon.perHashMetrics[hashNumber][metricNumber];
+   }
+  }
+ }
 }
 
 void simulationMonitor_emitMonitors() {
- //Find the species
  int speciesList[SPECIES_MAX_TYPES][3];
- simulationMonitor_findSpecies(speciesList);
+ //Gather the per-agent metrics
+ simulationMonitor_findPerAgentMetrics();
+ //Find the species
+ numberOfSpecies = simulationMonitor_findSpecies(speciesList);
+ if(numberOfSpecies <= 0)
+  return; //If no species found, just abort
  //Collect the metrics based on the species
+ simulationMonitor_collectSpeciesMetrics(speciesList, numberOfSpecies);
+ //Emit the metrics files
+ simulationMonitor_writeMetricsFiles();
+}
+
+void simulationMonitor_writeMetricsFiles(int numberOfSpecies) {
  int i, c, totalActions;
  float aveE, aveA, aveG;
  FILE *outFile;
- //Gather the stats to output 
- c = 0;
- aveE = 0;
- aveA = 0;
- for(i = 0; i < sm.w.numbAgents; i++) {
-  if(sm.w.agents[i].status == AG_STATUS_ALIVE) {
-   c++; 
-   aveE += sm.w.agents[i].energy;
-   aveA += sm.w.agents[i].age;
-   aveG += sm.w.agents[i].generation;
-  }
- }
  //Output the file
  outFile = fopen(MONITOR_FILE_LOC,"a");
+ fprintf(outFile,"time,");
+ simulationMonitor_writeTimeStamp(outFile); //TimeStamp doesn't write spaces so you'll need one of those
+ fprintf(outFile," treatment,%s",sm.treatment);
+ fprintf(outFile," agents,%i iterations,%lu aveGenerations,%f aveAge,%f aveEnergy,%f speed,%f speedD,%f speedA,%f speedS,%f",c,sm.i,aveG/(float)c,aveA/(float)c,aveE/(float)c,sm.smon.speed,sm.smon.speedDecision,sm.smon.speedAction,sm.smon.speedSeed);
+ for(speciesNumber = 0; speciesNumber < numberOfSpecies; speciesNumber++) {
+  //Per decision
+  fprintf(outFile," moves,%f",float(simulationMonitor_getMovesForSpecies(speciesNumber))/float(simulationMonitor_getDecisionsForSpecies(speciesNumber)));
+  fprintf(outFile," turns,%f",float(simulationMonitor_getTurnsForSpecies(speciesNumber))/float(simulationMonitor_getDecisionsForSpecies(speciesNumber)));
+  fprintf(outFile," attacks,%f",float(simulationMonitor_getAttacksForSpecies(speciesNumber))/float(simulationMonitor_getDecisionsForSpecies(speciesNumber)));
+  fprintf(outFile," grows,%f",float(simulationMonitor_getGrowsForSpecies(speciesNumber))/float(simulationMonitor_getDecisionsForSpecies(speciesNumber)));
+  fprintf(outFile," asexReplications,%f",float(simulationMonitor_getASexualReplicationsForSpecies(speciesNumber))/float(simulationMonitor_getDecisionsForSpecies(speciesNumber)));
+  fprintf(outFile," sexReplications,%f",float(simulationMonitor_getSexualReplicationsForSpecies(speciesNumber))/float(simulationMonitor_getDecisionsForSpecies(speciesNumber)));
+  //Decision specific
+  fprintf(outFile," moveFailed,%f",float(simulationMonitor_getFailedMovesForSpecies(speciesNumber))/float(simulationMonitor_getMovesForSpecies(speciesNumber)));
+  fprintf(outFile," replicationFailed,%f",float(simulationMonitor_getFailedReplicationForSpecies(speciesNumber))/float(simulationMonitor_getASexualReplicationsForSpecies(speciesNumber) +simulationMonitor_getSexualReplicationsForSpecies(speciesNumber)));
+  fprintf(outFile," attackFailed,%f",float(simulationMonitor_getFailedAttacksForSpecies(speciesNumber))/float(simulationMonitor_getAttacksForSpecies(speciesNumber)));
+  fprintf(outFile," growFailed,%f",float(simulationMonitor_getFailedGrowForSpecies(speciesNumber))/float(simulationMonitor_getGrowsForSpecies(speciesNumber)));
+  fprintf(outFile," killedBySeeding,%i",simulationMonitor_getKilledBySeedingForSpecies(speciesNumber));
+  fprintf(outFile," killedByAttacks,%i",simulationMonitor_getKilledByAttacksForSpecies(speciesNumber));
+  fprintf(outFile," killedByStarving,%i",simulationMonitor_getKilledByStarvingForSpecies(speciesNumber));
+  fprintf(outFile," newBrainSize,%i",simulationMonitor_getKilledBySeedingForSpecies(speciesNumber));
+  fprintf(outFile," killedBySeeding,%i",simulationMonitor_getKilledBySeedingForSpecies(speciesNumber));
+  fprintf(outFile," killedBySeeding,%i",simulationMonitor_getKilledBySeedingForSpecies(speciesNumber));
+  fprintf(outFile," killedBySeeding,%i",simulationMonitor_getKilledBySeedingForSpecies(speciesNumber));
+ }
+
+void simulationMonitor_addAveEnergyForHash(int hash, int x)
+ sm.smon.perHashMetrics[hash][16] += x;
+int simulationMonitor_getAveEnergyForSpecies(int species, int x)
+ return sm.smon.perSpeciesMetrics[hash][16];
+
+void simulationMonitor_addAveAgeForHash(int hash, int x)
+ sm.smon.perHashMetrics[hash][17] += x;
+int simulationMonitor_getAveAgeForSpecies(int species, int x)
+ return sm.smon.perSpeciesMetrics[hash][17];
+
+void simulationMonitor_addAveGenerationForHash(int hash, int x)
+ sm.smon.perHashMetrics[hash][18] += x;
+int simulationMonitor_getAveGenerationForSpecies(int species, int x)
+ return sm.smon.perSpeciesMetrics[hash][18];
+
+void simulationMonitor_addNumberOfAgentsForHash(int hash, int x)
+ sm.smon.perHashMetrics[hash][19] += x;
+int simulationMonitor_getNumberOfAgentsForSpecies(int species, int x)
+ return sm.smon.perSpeciesMetrics[hash][19];
+
+
  fprintf(outFile,"time,");
  simulationMonitor_writeTimeStamp(outFile); //TimeStamp doesn't write spaces so you'll need one of those
  fprintf(outFile," treatment,%s",sm.treatment);
--- simulator/simulationMonitor.h
+++ simulator/simulationMonitor.h
@@ -1,22 +1,33 @@
-#ifndef simulationmonitor_h 
-#define simulationmonitor_h 
+#ifndef simulationmonitor_h #define simulationmonitor_h 
 
+//Three kinds of metrics
+// per decision
+// - per birth
+// - per attack
+// - per etc.
+// per existing agent (these don't need to be gathered here, they get gathered in a snapshot)
 typedef struct simulationMonitor
 {
  int addedCon, didntAddCon, removedCon, didntRemoveCon;
  double speed, speedDecision, speedAction, speedSeed, speedIntelTests;
+
+ int perHashMetrics[AG_MAX_HASH][SPECIES_NUMBER_OF_METRICS];
+ int perSpeciesMetrics[SPECIES_TYPES_MAX][SPECIES_NUMBER_OF_METRICS];
+
+ // -- ALL HASHES METRICS -- 
  //Decisions 
- unsigned int decisions[AG_MAX_HASH];
- unsigned int moves[AG_MAX_HASH];
- unsigned int turns[AG_MAX_HASH];
- unsigned int attacks[AG_MAX_HASH];
- unsigned int grows[AG_MAX_HAS];
- unsigned int asexReplications[AG_MAX_HASH], sexReplications[AG_MAX_HASH];
- //Decision outcomes
- unsigned int failedMoves[AG_MAX_HAS], failedReplications[AG_MAX_HAS], failedAttacks[AG_MAX_HAS], failedGrows[AG_MAX_HAS];
- unsigned int killedBySeeding[AG_MAX_HAS], killedByAttacks[AG_MAX_HAS], killedByStarving[AG_MAX_HAS];
- //New agents
- unsigned int newBrainSize[AG_MAX_HAS], newBrains[AG_MAX_HAS];
+// unsigned int decisions[AG_MAX_HASH], moves[AG_MAX_HASH], turns[AG_MAX_HASH], attacks[AG_MAX_HASH], grows[AG_MAX_HASH], asexReplications[AG_MAX_HASH], sexReplications[AG_MAX_HASH];
+ //Decision specifics
+ //unsigned int failedMoves[AG_MAX_HASH], failedReplications[AG_MAX_HASH], failedAttacks[AG_MAX_HASH], failedGrows[AG_MAX_HASH], killedBySeeding[AG_MAX_HASH], killedByAttacks[AG_MAX_HASH], killedByStarving[AG_MAX_HASH], newBrainSize[AG_MAX_HASH], newBrains[AG_MAX_HASH];
+
+ // -- METRICS BY SPECIES --
+ //Decisions 
+ //unsigned int species_decisions[SPECIES_TYPES_MAX], species_moves[SPECIES_TYPES_MAX], species_turns[SPECIES_TYPES_MAX], species_attacks[SPECIES_TYPES_MAX], species_grows[SPECIES_TYPES_MAX], species_asexReplications[SPECIES_TYPES_MAX], species_sexReplications[SPECIES_TYPES_MAX];
+ //Decision specifics
+ //unsigned int species_failedMoves[SPECIES_TYPES_MAX], species_failedReplications[SPECIES_TYPES_MAX], species_failedAttacks[SPECIES_TYPES_MAX], species_failedGrows[SPECIES_TYPES_MAX], species_killedBySeeding[SPECIES_TYPES_MAX], species_killedByAttacks[SPECIES_TYPES_MAX], species_killedByStarving[SPECIES_TYPES_MAX], species_newBrainSize[SPECIES_TYPES_MAX], species_newBrains[SPECIES_TYPES_MAX];
+
+ // -- PER EXISTING AGENT METRICS BY SPECIES --
+ unsigned int species_aveEnergy[SPECIES_TYPES_MAX], species_aveAge[SPECIES_TYPES_MAX], species_aveGeneration[SPECIES_TYPES_MAX];
 } simulationMonitor; 
 
 int simulationMontior_test();
